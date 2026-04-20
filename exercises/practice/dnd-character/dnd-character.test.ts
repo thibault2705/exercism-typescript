3f3804a72eab15550afba95d36812f588fcb7a86
Thibault Phan <thibault2705@gmail.com>
2026-04-20 14:15:18 -0600

    Add missing tests for dnd-character exercise

--- exercises/practice/dnd-character/dnd-character.test.ts
+++ exercises/practice/dnd-character/dnd-character.test.ts
@@ -131,4 +131,15 @@ describe('Character creation', () => {
 
     expect(character.strength === character.strength).toBeTruthy()
   })
+
+  xit('Each ability is only calculated once', () => {
+    const character = new DnDCharacter()
+
+    expect(character.strength === character.strength).toBeTruthy()
+    expect(character.dexterity === character.dexterity).toBeTruthy()
+    expect(character.constitution === character.constitution).toBeTruthy()
+    expect(character.intelligence === character.intelligence).toBeTruthy()
+    expect(character.wisdom === character.wisdom).toBeTruthy()
+    expect(character.charisma === character.charisma).toBeTruthy()
+  })
 })
